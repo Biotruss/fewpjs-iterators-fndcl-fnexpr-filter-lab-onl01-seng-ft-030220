e8d4971b8dd9f72ab3afccd4003b2b7dd3818025
Craig Christopher Messi <craig.messi@gmail.com>
2020-06-23 04:12:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const findMatching = (drivers, name) => {
   return drivers.filter((driver) => driver.toLowerCase() === name.toLowerCase())
 }
 
-//const fuzzyMatch = (drivers, pattern) => {
-//  return drivers.filter(driver => driver.startsWith(pattern))
-//}
+const fuzzyMatch = (drivers, pattern) => {
+  return drivers.filter(driver => driver.startsWith(pattern))
+  
+}
 
-//const matchName = (drivers, name) => {
-//  return drivers.filter(driverObj => driverObj.name === name)
-//}
+const matchName = (drivers, name) => {
+  return drivers.filter(driverObj => driverObj.name === name)
+  
+}
